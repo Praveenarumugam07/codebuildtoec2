248a7604584001c8c5be4ac2207c89ec01f14a32
Praveenarumugam07 <96639707+Praveenarumugam07@users.noreply.github.com>
2025-06-26 16:35:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-python-app',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(),
     install_requires=[
         'flask'
